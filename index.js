661f9f944a0dda618d55b4c7829251d34cf25fdc
Sidoine De Wispelaere <sdewispelaere@talentsoft.com>
2017-10-11 17:07:43 +0200

    Initial import

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.abs = Math.abs;
+exports.acos = Math.acos;
+exports.asin = Math.asin;
+exports.atan = Math.atan;
+exports.atan2 = Math.atan2;
+exports.ceil = Math.ceil;
+exports.cos = Math.cos;
+exports.cosh = Math.cosh;
+function deg(x) {
+    return x * Math.PI / 180;
+}
+exports.deg = deg;
+exports.exp = Math.exp;
+exports.floor = Math.floor;
+function fmod(x, y) {
+    return x % y;
+}
+exports.fmod = fmod;
+exports.huge = Number.MAX_VALUE;
+exports.log = Math.log;
+exports.log10 = Math.log10;
+exports.max = Math.max;
+exports.min = Math.min;
+exports.pi = Math.PI;
+exports.nan = NaN;
---
+++ index.ts
@@ -0,0 +1,23 @@
+export const abs = Math.abs;
+export const acos = Math.acos;
+export const asin = Math.asin;
+export const atan = Math.atan;
+export const atan2 = Math.atan2;
+export const ceil = Math.ceil;
+export const cos = Math.cos;
+export const cosh = Math.cosh;
+export function deg(x: number) {
+    return x * Math.PI / 180;
+}
+export const exp = Math.exp;
+export const floor = Math.floor;
+export function fmod(x: number, y: number) {
+    return x % y;
+}
+export const huge = Number.MAX_VALUE;
+export const log = Math.log;
+export const log10 = Math.log10;
+export const max = Math.max;
+export const min = Math.min;
+export const pi = Math.PI;
+export const nan = NaN;
